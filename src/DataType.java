529213968b1d3f7edbe0c8190091266eb0763369
Ian Macharia <karianmash@karianpc>
2023-10-19 14:00:56 +0300

    Primitive data types in Java

---
+++ src/DataType.java
@@ -0,0 +1,29 @@
+public class DataType {
+
+    public static void main(String[] args) {
+        // Integer
+        int a = 10;
+        System.out.println("This is an integer: " + a);
+
+        // Float
+        float b = 10.5f;
+        System.out.println("This is a float: " + b);
+
+        // Double - more precise than float
+        double c = 10.5;
+        System.out.println("This is a double: " + c);
+
+        // Boolean
+        boolean d = true;
+        System.out.println("This is a boolean: " + d);
+
+        // Character
+        char e = 'H';
+        System.out.println("This is a character: " + e);
+
+        // String
+        String f = "Hello World";
+        System.out.println("This is a string: " + f);
+    }
+
+}
